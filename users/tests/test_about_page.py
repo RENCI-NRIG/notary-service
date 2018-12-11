e751bbc4ee8a55417528d9f4c1c4d709a0811161
Michael J. Stealey <michael.j.stealey@gmail.com>
2018-12-11 12:30:15 -0500

    test cleanup
     Clean up tests based on pytest warning output

--- users/tests/test_about_page.py
+++ users/tests/test_about_page.py
@@ -9,15 +9,15 @@ class AboutPageTests(SimpleTestCase):
 
     def test_about_page_status_code(self):
         response = self.client.get('/about/')
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_view_url_by_name(self):
         response = self.client.get(reverse('about'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('about'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'about.html')
 
     # def test_about_page_contains_correct_html(self):
--- users/tests/test_contact_page.py
+++ users/tests/test_contact_page.py
@@ -9,15 +9,15 @@ class ContactPageTests(SimpleTestCase):
 
     def test_contact_page_status_code(self):
         response = self.client.get('/contact/')
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_view_url_by_name(self):
         response = self.client.get(reverse('contact'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('contact'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contact.html')
 
     # def test_contact_page_contains_correct_html(self):
