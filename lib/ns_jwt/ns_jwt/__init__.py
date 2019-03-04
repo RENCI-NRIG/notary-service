8064c6184b324600fa3d33c1a7856900525a2084
Michael J. Stealey <michael.j.stealey@gmail.com>
2019-03-04 16:53:00 -0500

    prep for pypi
    update files for inclusion on PyPi and publish as ns-jwt

--- lib/ns_jwt/ns_jwt/__init__.py
+++ lib/ns_jwt/ns_jwt/__init__.py
@@ -1,1 +1,3 @@
 from .ns_jwt import NSJWT, NSJWTError
+
+name = "ns_jwt"
--- lib/ns_jwt/setup.py
+++ lib/ns_jwt/setup.py
@@ -1,16 +1,28 @@
-from setuptools import setup
+import setuptools
 
-setup(name='ns_jwt',
-      version='0.1',
-      description='Notary Service support for JWT',
-      url='https://github.com/RENCI-NRIG/notary-service',
-      author='Ilya Baldin',
-      author_email='ibaldin@renci.org',
-      license='MIT',
-      packages=['ns_jwt'],
-      install_requires=[
-          'PyJWT',
-          'cryptography',
-          'python-dateutil',
-      ],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ns_jwt',
+    version='0.1.0',
+    author='Ilya Baldin',
+    author_email='ibaldin@renci.org',
+    description='Notary Service support for JWT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/RENCI-NRIG/notary-service',
+    install_requires=[
+        'PyJWT',
+        'cryptography',
+        'python-dateutil',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    zip_safe=False
+)
