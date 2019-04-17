ca6f50a50d81ec33b90dd29f03209a4da2c92ef9
Michael J. Stealey <michael.j.stealey@gmail.com>
2019-04-17 15:32:34 -0400

    #41 url slug update
    remove trailing / from URLs when it shouldn't be there

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('search/', views.search, name='search'),
-    path('profile/', views.profile, name='profile'),
-    path('login/', views.login, name='login'),
-    path('faq/', views.faq, name='faq'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('search', views.search, name='search'),
+    path('profile', views.profile, name='profile'),
+    path('login', views.login, name='login'),
+    path('faq', views.faq, name='faq'),
 ]
