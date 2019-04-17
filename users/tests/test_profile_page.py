0000dea5a01d0027d4abee70bd9bddbc2a32b5e1
Michael J. Stealey <michael.j.stealey@gmail.com>
2019-04-17 15:39:59 -0400

    #41 test update for new slugs
    update tests to look for slugs that no longer have a trailing /

--- users/tests/test_profile_page.py
+++ users/tests/test_profile_page.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class ProfilePageTests(SimpleTestCase):
 
     def test_profile_page_status_code(self):
-        response = self.client.get('/profile/')
+        response = self.client.get('/profile')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_by_name(self):
@@ -16,10 +16,6 @@ class ProfilePageTests(SimpleTestCase):
         response = self.client.get(reverse('profile'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'profile.html')
-
-    # def test_profile_page_contains_correct_html(self):
-    #     response = self.client.get('/about/')
-    #     self.assertContains(response, '<h1>Profile page</h1>')
 
     def test_profile_page_does_not_contain_incorrect_html(self):
         response = self.client.get('/')
