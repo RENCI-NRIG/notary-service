21819b24edd71230a84ce10275ba07d06fdbc14f
Yaxue Guo <yaxue.guo23@gmail.com>
2020-08-04 17:48:01 -0400

    completed merging certificate page into one section for user profile page

--- users/views.py
+++ users/views.py
@@ -57,55 +57,54 @@ def authresponse(request):
         }
     )
 
-
-def certificate(request):
-    """
-    Generate and retrieve certificate files from CILogon
-    If certificate files are generated, give the user one chance to download them, otherwise present the
-    certificate creation options
-    :param request:
-    :return:
-    """
-    if request.user.is_authenticated:
-        user = get_object_or_404(NotaryServiceUser, id=request.user.id)
-        auth_url = get_authorization_url()
-        certificate_files = ''
-        if request.method == 'POST':
-            if request.POST.get("download"):
-                if request.POST.get("path-cilogon.crt"):
-                    path = request.POST.get("path-cilogon.crt")
-                elif request.POST.get("path-cilogon.key"):
-                    path = request.POST.get("path-cilogon.key")
-                else:
-                    path = request.POST.get("path-cilogon.p12")
-                return download(request, path=path)
-            form = CILogonCertificateForm(request.POST)
-            if form.is_valid():
-                if request.POST.get("generate-certificate"):
-                    if str(request.POST.get('use_my_key')) == "True":
-                        # TODO allow user to upload private key for CSR generation
-                        pass
-                    else:
-                        certificate_files = generate_cilogon_certificates(
-                            user=user,
-                            authorization_response=str(request.POST.get("authorization_response")),
-                            p12_password=str(request.POST.get("p12_password")),
-                        )
-                        user.cilogon_certificate_date = timezone.now()
-                        user.save()
-        else:
-            form = CILogonCertificateForm()
-        return render(
-            request, 'certificate.html',
-            {
-                'profile_page': 'active',
-                'form': form,
-                'auth_url': auth_url,
-                'certificate_files': certificate_files
-            }
-        )
-    else:
-        return render(request, 'certificate.html', {'profile_page': 'active'})
+# def certificate(request):
+#     """
+#     Generate and retrieve certificate files from CILogon
+#     If certificate files are generated, give the user one chance to download them, otherwise present the
+#     certificate creation options
+#     :param request:
+#     :return:
+#     """
+#     if request.user.is_authenticated:
+#         user = get_object_or_404(NotaryServiceUser, id=request.user.id)
+#         auth_url = get_authorization_url()
+#         certificate_files = ''
+#         if request.method == 'POST':
+#             if request.POST.get("download"):
+#                 if request.POST.get("path-cilogon.crt"):
+#                     path = request.POST.get("path-cilogon.crt")
+#                 elif request.POST.get("path-cilogon.key"):
+#                     path = request.POST.get("path-cilogon.key")
+#                 else:
+#                     path = request.POST.get("path-cilogon.p12")
+#                 return download(request, path=path)
+#             form = CILogonCertificateForm(request.POST)
+#             if form.is_valid():
+#                 if request.POST.get("generate-certificate"):
+#                     if str(request.POST.get('use_my_key')) == "True":
+#                         # TODO allow user to upload private key for CSR generation
+#                         pass
+#                     else:
+#                         certificate_files = generate_cilogon_certificates(
+#                             user=user,
+#                             authorization_response=str(request.POST.get("authorization_response")),
+#                             p12_password=str(request.POST.get("p12_password")),
+#                         )
+#                         user.cilogon_certificate_date = timezone.now()
+#                         user.save()
+#         else:
+#             form = CILogonCertificateForm()
+#         return render(
+#             request, 'certificate.html',
+#             {
+#                 'profile_page': 'active',
+#                 'form': form,
+#                 'auth_url': auth_url,
+#                 'certificate_files': certificate_files
+#             }
+#         )
+#     else:
+#         return render(request, 'certificate.html', {'profile_page': 'active'})
 
 
 def set_role_boolean(user):
@@ -123,28 +122,58 @@ def set_role_boolean(user):
 def profile(request):
     if request.user.is_authenticated:
         user = get_object_or_404(NotaryServiceUser, id=request.user.id)
+        # for user profile
         ismemberof = IsMemberOf.objects.filter(
             membershipismemberof__user_id=request.user.id).order_by('value')
         ldapother = LdapOther.objects.filter(
             membershipldapother__user_id=request.user.id).order_by('attribute', 'value')
+        # for cilogon certificate
+        auth_url = get_authorization_url()
+        certificate_files = ''
         if request.method == "POST":
-            form = UserPreferences(request.POST, instance=user, user=request.user)
-            if form.is_valid():
+            # for user profile
+            preference_form = UserPreferences(request.POST, instance=user, user=request.user)
+            if preference_form.is_valid():
                 messages.success(request, 'Preferences changed successfully!')
                 if request.POST.get("delete-message"):
                     message = get_object_or_404(Message, uuid=request.POST.get('remove_message_uuid'))
                     message.is_active = False
                     message.save()
                 if request.POST.get("update-preferences"):
-                    user.show_uuid = form.data.get('show_uuid')
-                    user.role = form.data.get('role')
+                    user.show_uuid = preference_form.data.get('show_uuid')
+                    user.role = preference_form.data.get('role')
                     user.save()
                     set_role_boolean(user=user)
                 if request.POST.get("check-messages"):
                     check_for_new_messages(str(request.user.uuid))
                 return redirect('profile')
+            if request.POST.get("download"):
+                if request.POST.get("path-cilogon.crt"):
+                    path = request.POST.get("path-cilogon.crt")
+                elif request.POST.get("path-cilogon.key"):
+                    path = request.POST.get("path-cilogon.key")
+                else:
+                    path = request.POST.get("path-cilogon.p12")
+                return download(request, path=path)
+            # for cilogon certificate
+            certificate_form = CILogonCertificateForm(request.POST or None)
+            if certificate_form.is_valid():
+                if request.POST.get("generate-certificate"):
+                    if str(request.POST.get('use_my_key')) == "True":
+                        # TODO allow user to upload private key for CSR generation
+                        pass
+                    else:
+                        certificate_files = generate_cilogon_certificates(
+                            user=user,
+                            authorization_response=str(request.POST.get("authorization_response")),
+                            p12_password=str(request.POST.get("p12_password")),
+                        )
+                        user.cilogon_certificate_date = timezone.now()
+                        user.save()
         else:
-            form = UserPreferences(instance=user, user=request.user)
+            preference_form = UserPreferences(instance=user, user=request.user)
+            certificate_form = CILogonCertificateForm(request.POST)
+        
         ns_messages = Message.objects.filter(
             kafka_topic=str(request.user.uuid),
             is_active=True
@@ -153,7 +182,8 @@ def profile(request):
                       {'profile_page': 'active',
                        'isMemberOf': ismemberof,
                        'LDAPOther': ldapother,
-                       'form': form,
+                       'preference_form': preference_form,
+                       'certificate_form': certificate_form,
                        'role': get_object_or_404(Role, id=user.role).get_id_display(),
                        'ns_messages': ns_messages})
     else:
