45b210f0d27b91c4b86048b0e4837a8f38c7897d
Yaxue Guo <yaxue.guo23@gmail.com>
2020-08-27 13:37:50 -0400

    #80: completed nav elements update

--- apache_kafka/__init__.py
+++ apache_kafka/__init__.py
@@ -6,5 +6,6 @@ producer = KafkaProducer(
     bootstrap_servers=settings.KAFKA_SERVERS,
     key_serializer=lambda m: json.dumps(m).encode('ascii'),
     value_serializer=lambda m: json.dumps(m).encode('ascii'),
+    api_version=(0, 10, 1),
     retries=5
 )
